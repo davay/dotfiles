dd4187e4d7f93241df41c2fd0a2a3ddaf3669fd7
Devin Lim <davay@devinl.im>
2024-10-13 02:09:01 -0700

    lsp keybinds

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -19,8 +19,6 @@ vim.keymap.set('n', '<leader>ls', ':Leet submit<CR>', {})
 vim.keymap.set('n', '<leader>lm', ':Leet<CR>', {})
 vim.keymap.set('n', '<leader>lc', ':Leet console<CR>', {})
 
--- if you work with html outputs:
-vim.keymap.set("n", "<localleader>mx", ":MoltenOpenInBrowser<CR>", { desc = "open output in browser", silent = true })
 
 -- quarto & molten for jupyter
 local runner = require("quarto.runner")
@@ -33,6 +31,8 @@ vim.keymap.set("n", "<leader>[", ':MoltenPrev<CR>', { desc = "jump to prev cell"
 vim.keymap.set("n", "<leader>]", ':MoltenNext<CR>', { desc = "jump to next cell", silent = true })
 vim.keymap.set("n", "<leader>mr", ':MoltenRestart<CR>', { desc = "restart kernel", silent = true })
 vim.keymap.set("n", "<leader>rr", ':MoltenRestart!<CR>', { desc = "restart kernel and clear all", silent = true })
+-- if you work with html outputs:
+vim.keymap.set("n", "<localleader>mx", ":MoltenOpenInBrowser<CR>", { desc = "open output in browser", silent = true })
 
 -- outline
 vim.keymap.set("n", "<leader>o", ":Outline!<CR>", { desc = "Toggle Outline" })
@@ -40,7 +40,7 @@ vim.keymap.set("n", "<leader>o", ":Outline!<CR>", { desc = "Toggle Outline" })
 -- use tab to switch to next pane
 vim.api.nvim_set_keymap('n', '<Tab>', '<C-w>w', { noremap = true, silent = true })
 
--- toggle between current and previous buffer
+-- toggle between current and previous buffer (backspace)
 vim.api.nvim_set_keymap('n', '<bs>', '<c-^>zz', { noremap = true, silent = true })
 
 -- make new panes
@@ -62,6 +62,7 @@ vim.api.nvim_set_keymap('n', '<leader>n', ':Neotree action=show toggle<CR>', { n
 -- treesitter keymaps in plugins/treesitter.lua
 ----
 
+-- luasnip
 local ls = require("luasnip")
 vim.keymap.set({ "i", "x" }, "<C-j>", function()
   if ls.expand_or_jumpable() then
@@ -86,3 +87,56 @@ vim.keymap.set({ "i", "x" }, "<C-h>", function()
     ls.change_choice(-1)
   end
 end, { silent = true, desc = "previous snippet choice" })
+
+
+-- lsp
+vim.keymap.set('n', '<leader>dd', function()
+  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, { silent = true, noremap = true, desc = "toggle lsp diagnostics" })
+
+-- floating keybinding help for lsp
+_G.show_lsp_zero_keybindings = function()
+  local keybindings = {
+    { key = "K",  desc = "Hover Documentation" },
+    { key = "gd", desc = "Go to Definition" },
+    { key = "gD", desc = "Go to Declaration" },
+    { key = "gi", desc = "Go to Implementation" },
+    { key = "go", desc = "Go to Type Definition" },
+    { key = "gr", desc = "List References" },
+    { key = "gs", desc = "Display Signature" },
+    { key = "F2", desc = "Rename" },
+    { key = "F3", desc = "Format" },
+    { key = "F4", desc = "Code Action" },
+    { key = "[d", desc = "Previous Diagnostic" },
+    { key = "]d", desc = "Next Diagnostic" },
+  }
+
+  local lines = { "LSP-Zero Keybindings:" }
+  for _, binding in ipairs(keybindings) do
+    table.insert(lines, string.format("%s: %s", binding.key, binding.desc))
+  end
+
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+
+  local width = 40
+  local height = #lines
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = width,
+    height = height,
+    col = (vim.o.columns - width) / 2,
+    row = (vim.o.lines - height) / 2,
+    style = "minimal",
+    border = "rounded",
+  })
+
+  -- Close the window when pressing 'q' or '<Esc>'
+  vim.api.nvim_buf_set_keymap(buf, 'n', 'q', ':close<CR>', { noremap = true, silent = true })
+  vim.api.nvim_buf_set_keymap(buf, 'n', '<Esc>', ':close<CR>', { noremap = true, silent = true })
+end
+
+vim.api.nvim_set_keymap('n', '<leader>/', ':lua _G.show_lsp_zero_keybindings()<CR>', { noremap = true, silent = true })
+
+-- oil
+vim.keymap.set('n', '<leader>e', ':Oil<CR>', { silent = true, noremap = true })
